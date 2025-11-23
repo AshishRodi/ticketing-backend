6474f9ac37ee766996095c78ec7a1046f9e37477
Sanjiv Rodi <sanjivrodi@Sanjivs-MacBook-Air.local>
2025-11-23 13:38:02 +0530

    email service next 2

--- utils/emailService.js
+++ utils/emailService.js
@@ -1,24 +1,24 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
+  host: "smtp.elasticemail.com",
+  port: 2525,
+  secure: false,
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
+    user: "invoiceprocessing639@gmail.com",
+    pass: "8A884BF43C62E31462199F7CA6F0EE3B2048"
+  }
 });
 
 exports.sendEmail = async (to, subject, html) => {
   try {
-    const info = await transporter.sendMail({
-      from: process.env.FROM_EMAIL,
+    await transporter.sendMail({
+      from: "invoiceprocessing639@gmail.com",
       to,
       subject,
-      html,
+      html
     });
-
-    console.log("Email sent:", info.messageId);
+    console.log("Email sent successfully");
   } catch (err) {
     console.error("Email sending error:", err);
   }
